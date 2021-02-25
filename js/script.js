38656338c11eb1ed49efeb78ff9092a47397b2d9
Mainul Murad <mainulmurad486@gmail.com>
2021-02-25 21:19:29 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	//wow js
+  new WOW().init();
+  //slick
+  $('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+ 
+
+});
+
+
